2c6ebc0546be7a2b6171b27d9736163385a9fd99
Howard HUANG <hhuang63@ds2.CS.McGill.CA>
2017-09-22 16:17:11 -0400

    LSTM

--- Flask/model.py
+++ Flask/model.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, LSTM
 
 def get_model():
     model = Sequential()
@@ -7,4 +7,10 @@ def get_model():
     model.add(Activation('relu'))
     model.add(Dense(12))
     model.add(Activation('softmax'))
+    return model
+
+def get_lstm_model(): #???
+    model = Sequential()
+    model.add(LSTM(32, input_shape=(1,), return_sequences=True))
+    model.add(LSTM(12))
     return model
