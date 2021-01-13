2f170ac3076f075e99235ec0400d484abbbb7a67
Howard Huang <howard.huang@mail.mcgill.ca>
2021-01-13 05:58:43 -0500

    Collate function pads to longest sequence

--- music/train.py
+++ music/train.py
@@ -362,6 +362,63 @@ def notes_to_midi(notes, win_length, hop_length, rate):
 # Experiment
 ##################################################
 
+from torch.utils.data._utils.collate import default_collate
+from torch._six import container_abcs, string_classes, int_classes
+
+def collate_fn(batch):
+    r"""Puts each data field into a tensor with outer dimension batch size"""
+
+    elem = batch[0]
+    elem_type = type(elem)
+    if isinstance(elem, torch.Tensor):
+		# --------------------------------------------------
+        shapes = torch.tensor([e.shape for e in batch])
+        max_shape = shapes.max(0).values
+        output = [torch.zeros(max_shape.numpy().tolist()) for _ in batch]
+        for o,e in zip(output,batch):
+            s = [slice(0,l) for l in e.shape]
+            o[s] = e
+        batch = output
+		# --------------------------------------------------
+        out = None
+        if torch.utils.data.get_worker_info() is not None:
+            # If we're in a background process, concatenate directly into a
+            # shared memory tensor to avoid an extra copy
+            numel = sum([x.numel() for x in batch])
+            storage = elem.storage()._new_shared(numel)
+            out = elem.new(storage)
+        return torch.stack(batch, 0, out=out)
+    elif elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
+            and elem_type.__name__ != 'string_':
+        if elem_type.__name__ == 'ndarray' or elem_type.__name__ == 'memmap':
+            # array of string classes and object
+            if np_str_obj_array_pattern.search(elem.dtype.str) is not None:
+                raise TypeError(default_collate_err_msg_format.format(elem.dtype))
+
+            return collate_fn([torch.as_tensor(b) for b in batch])
+        elif elem.shape == ():  # scalars
+            return torch.as_tensor(batch)
+    elif isinstance(elem, float):
+        return torch.tensor(batch, dtype=torch.float64)
+    elif isinstance(elem, int_classes):
+        return torch.tensor(batch)
+    elif isinstance(elem, string_classes):
+        return batch
+    elif isinstance(elem, container_abcs.Mapping):
+        return {key: collate_fn([d[key] for d in batch]) for key in elem}
+    elif isinstance(elem, tuple) and hasattr(elem, '_fields'):  # namedtuple
+        return elem_type(*(collate_fn(samples) for samples in zip(*batch)))
+    elif isinstance(elem, container_abcs.Sequence):
+        # check to make sure that the elements in batch have consistent size
+        it = iter(batch)
+        elem_size = len(next(it))
+        if not all(len(elem) == elem_size for elem in it):
+            raise RuntimeError('each element in list of batch should be of equal size')
+        transposed = zip(*batch)
+        return [collate_fn(samples) for samples in transposed]
+
+    raise TypeError(default_collate_err_msg_format.format(elem_type))
+
 class MusicNetExperiment(Experiment):
     def setup(self, config):
         output_dir = config.pop('output_dir','./output')
@@ -502,9 +559,11 @@ class MusicNetExperimentRNN(Experiment):
         batch_size = config.pop('batch_size',64)
         train_dataset, val_dataset = get_datasets_recurrent(musicnet_dir, window_size=window_size, seq_len=seq_len,stride=self.stride)
         self.train_dataloader = torch.utils.data.DataLoader(
-                dataset=train_dataset, batch_size=batch_size, shuffle=True)
+                dataset=train_dataset, batch_size=batch_size, shuffle=True,
+                collate_fn=collate_fn)
         self.val_dataloader = torch.utils.data.DataLoader(
-                dataset=val_dataset, batch_size=batch_size, shuffle=False)
+                dataset=val_dataset, batch_size=batch_size, shuffle=False,
+                collate_fn=collate_fn)
 
         self.state['best_model_train'] = copy.deepcopy(self.net)
         self.state['best_loss_train'] = float('inf')
@@ -615,7 +674,8 @@ def train(output_dir='./output', best_model_file_name=None,
 def train_rnn(output_dir='./output', best_model_file_name=None,
         checkpoint_file_name=None, musicnet_dir=None):
     config = {
-        'batch_size': 64
+        'batch_size': 64,
+        'musicnet_dir': musicnet_dir
     }
     exp = MusicNetExperimentRNN(
             epoch=5,
